a9c9d193eb8d4c4ec2de34445e00166a4d85d673
WGY1118 <2018909795@qq.com>
2019-07-25 14:52:56 +0800

    修改了UserController30行代码

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -28,6 +28,7 @@ public class UserController {
      * */
     @RequestMapping("userList")
     public Object list(){
+        System.out.println("222");
         return userService.getList();
     }
 
